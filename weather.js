ad5e5b32fb1e26e21a6e41a6498346971139dde5
Eric Devitt <sailingrules@msn.com>
2022-06-27 22:00:10 -0400

    adding code to repo

---
+++ weather.js
@@ -0,0 +1,11 @@
+const axios = require('axios');
+
+axios
+.get('https://api.weather.gov')
+.then(res => {
+    console.log('statusCode: ${res.status}');
+    console.log(res);
+})
+.catch(error => {
+    console.error(error);
+});
