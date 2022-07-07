c746a4ce9e28057edd2fd39909e776cc5451f069
Eric Devitt <sailingrules@msn.com>
2022-07-07 06:01:24 -0400

    make work is pushed

--- weather.js
+++ weather.js
@@ -2,10 +2,10 @@
 const axios = require('axios');
 
  axios
- .get(`https://api.weather.gov`)
+ .get(`https://api.weather.gov/points/39.7456,-97.0892`)
 .then(res => {
      console.log(`statusCode: ${res.status}`);
-     console.log(res);
+     console.log(res.body);
  })
  .catch(error => {
     console.error(error);
