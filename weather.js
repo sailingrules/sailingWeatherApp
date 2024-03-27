52123d897113d546a1169ac174eaa649e4bb7e2b
sailingrules <61519882+sailingrules@users.noreply.github.com>
2024-03-27 19:41:33 -0400

    Update weather.js

--- weather.js
+++ weather.js
@@ -13,24 +13,24 @@
 
 // Express server    for node running this code must not be commented out...the front end code must be commented out...then in terminal enter" npm run start" 
 
- let express = require(`express`);
- let app = express();
+ // let express = require(`express`);
+ // let app = express();
 
- let router = express.Router();
+ // let router = express.Router();
 
- router.get('/', function(req,res,next){
-        res.send("sweet");
- });
+ // router.get('/', function(req,res,next){
+ //        res.send("sweet");
+ // });
 
 //  app.use('/api/', router);
 //  let port = process.env.PORT;
 //  if (port == null || port == "") {
 //    port = 8000;
  
-    app.listen(port);
-   var server = app.listen (80,function(){
-     console.log('Node server is running');
- });
+ //    app.listen(port);
+ //   var server = app.listen (80,function(){
+ //     console.log('Node server is running');
+ // });
 
  //Search verification 
 // const searchBtn = document.getElementById("search");
