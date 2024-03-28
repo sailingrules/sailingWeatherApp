fcf08c6ef26063abab5eedf700ff7d6b94d6e59a
sailingrules <61519882+sailingrules@users.noreply.github.com>
2024-03-27 21:59:32 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const app = require("express")();
-const path = require('path'):
+
 const PORT = process.env.PORT || 80;
 
 app.get("", (req,res) => {
-    res.send("Hello Everyone");
+    res.send("Hello World");
  });
-app.get('/', async(req,res) => { res.sendFile(path.join(_ _dirname,'sailingweatherapp', 'index.html'));
 app.listen(PORT, () => {
 console.log('App up at port ${PORT}');
 });
